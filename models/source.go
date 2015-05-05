0ef466f1fe664dbef433cfcd75db9cb105769938
Matthew Bucci <buccim2@gene.com>
2015-05-05 08:18:35 -0700

    subreddit should update too

--- models/source.go
+++ models/source.go
@@ -1,21 +1,23 @@
 package models
 
 import (
+	"fmt"
 	db "github.com/AlienStream/Shared-Go/database"
 	mysql "github.com/ziutek/mymysql/mysql"
 	"time"
 )
 
 type Source struct {
-	Id          int
-	Title       string
-	Description string
-	Type        string
-	Importance  int
-	Url         string
-	Thumbnail   string
-	Updated_at  time.Time
-	Created_at  time.Time
+	Id                int
+	Title             string
+	Description       string
+	Type              string
+	Importance        int
+	Url               string
+	Thumbnail         string
+	Updated_at        time.Time
+	Created_at        time.Time
+	Refresh_frequency int
 }
 
 func AllSources() []Source {
@@ -27,8 +29,22 @@ func AllSources() []Source {
 	return RowsToSources(rows)
 }
 
-func (Source) Save() {
-
+func (s Source) Save() {
+	if s.Id == 0 {
+		fmt.Printf("Inserting New Source %s \n", s.Title)
+		stmt, err := db.Con.Prepare("insert into sources (`title`, `description`, `type`, `importance`, `url`, `thumbnail`, `created_at`, `updated_at`, `refresh_frequency`) values (?, ?, ?, ?, ?, ?, ?, ?, ?)")
+		if err != nil {
+			panic(err)
+		}
+		stmt.Exec(s.Title, s.Description, s.Type, s.Importance, s.Url, s.Thumbnail, time.Now(), time.Now(), s.Refresh_frequency)
+	} else {
+		fmt.Printf("Updating Source %s \n", s.Title)
+		stmt, err := db.Con.Prepare("update sources set `title`=?, `description`=?, `type`=?, `importance`=?, `url`=?, `thumbnail`=?, `updated_at`=?, `refresh_frequency`=? where `id`=?")
+		if err != nil {
+			panic(err)
+		}
+		stmt.Exec(s.Title, s.Description, s.Type, s.Importance, s.Url, s.Thumbnail, time.Now(), s.Refresh_frequency, s.Id)
+	}
 }
 
 func RowsToSources(rows []mysql.Row) []Source {
@@ -49,5 +65,6 @@ func (s Source) FromRow(row mysql.Row) Source {
 	s.Thumbnail = row.Str(6)
 	s.Updated_at = row.Localtime(7)
 	s.Created_at = row.Localtime(8)
+	s.Refresh_frequency = row.Int(9)
 	return s
 }
