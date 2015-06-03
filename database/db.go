dab8e1dbd227aeb6a2dcc2110855d039a1b6c6cb
Matthew Bucci <mtbucci@gmail.com>
2015-06-03 04:17:17 -0400

    pull db from env variables if not on dev

--- database/db.go
+++ database/db.go
@@ -3,16 +3,32 @@ package database
 import (
 	"github.com/ziutek/mymysql/autorc"
 	_ "github.com/ziutek/mymysql/thrsafe"
+	"os"
 	//sq "github.com/lann/squirrel"
 )
-
-const database = "homestead"
-const host = "127.0.0.1:33060"
-const user = "homestead"
-const password = "secret"
 
 var Con *autorc.Conn
 
 func init() {
+	var database string = os.Getenv("DB_NAME") 
+	if database == "" {
+		database = "homestead"
+	}
+
+	var host string = os.Getenv("DB_HOST") 
+	if host == "" {
+		host = "127.0.0.1:33060"
+	}
+
+	var user string = os.Getenv("DB_USER")
+	if user == "" {
+		 user = "homestead"
+	}
+	
+	var password string = os.Getenv("DB_PASSWORD")
+	if password == "" {
+		password = "secret"
+	}
+
 	Con = autorc.New("tcp", "", host, user, password, database)
 }
